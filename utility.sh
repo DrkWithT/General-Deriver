9f925f81e281038267800a11c9b3c0c31b070ef6
drkwitht <derektan3510@gmail.com>
2024-09-18 23:19:02 -0700

    Setup project.

---
+++ utility.sh
@@ -0,0 +1,20 @@
+# utility.sh
+# wraps CMake invocations
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: ./utility.sh [info | debug | release]"
+    exit 1
+fi
+
+option="$1"
+
+if [[ "$option" = "debug" ]]; then
+    cmake --fresh -S . -B build --preset vsc-gcc-debug-config && cmake --build build && cp ./build/compile_commands.json .
+elif [[ "$option" = "release" ]]; then
+    cmake --fresh -S . -B build --preset vsc-gcc-release-config && cmake --build build && cp ./build/compile_commands.json .
+elif [[ "$option" = "info" ]]; then
+    echo "Usage: ./utility.sh [info | debug | release]"
+else
+    echo "Invalid option!"
+    exit 1
+fi
