a2604539fd5f99a889989e541bb81255be298b18
drkwitht <derektan3510@gmail.com>
2024-09-20 20:24:57 -0700

    Fixed CTest setup.

--- utility.sh
+++ utility.sh
@@ -2,7 +2,7 @@
 # Brief: wraps CMake invocations
 
 if [[ $# -ne 1 ]]; then
-    echo "Usage: ./utility.sh [info | debug | release]"
+    echo "Usage: ./utility.sh [info | debug | release | test]"
     exit 1
 fi
 
@@ -13,7 +13,9 @@ if [[ "$option" = "debug" ]]; then
 elif [[ "$option" = "release" ]]; then
     cmake --fresh -S . -B build --preset vsc-gcc-release-config && cmake --build build && cp ./build/compile_commands.json .
 elif [[ "$option" = "info" ]]; then
-    echo "Usage: ./utility.sh [info | debug | release]"
+    echo "Usage: ./utility.sh [info | debug | release | test]"
+elif [[ "$option" = "test" ]]; then
+    ctest --test-dir build
 else
     echo "Invalid option!"
     exit 1
