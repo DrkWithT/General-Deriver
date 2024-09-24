a7b0a7e1389b4226b2a8b95434ef10091ebf1c57
drkwitht <derektan3510@gmail.com>
2024-09-23 23:09:14 -0700

    Added AST types.

---
+++ src/include/Syntax/IAstNode.hpp
@@ -0,0 +1,38 @@
+#ifndef I_AST_NODE_HPP
+#define I_AST_NODE_HPP
+
+#include <any>
+#include "Syntax/IAstVisitor.hpp"
+
+namespace GeneralDeriver::Syntax {
+    /// @note Represents only supported operations binding sub-expressions by x.
+    enum class AstOpType : uint8_t {
+        add,
+        sub,
+        mul,
+        div,
+        none
+    };
+
+    /// @note Denotes only types of expressions e.g constants.
+    enum class AstNodeType : uint8_t {
+        literal,
+        unary,
+        binary
+    };
+
+    /**
+     * @brief Represents a syntactic construct of a parsed function body of x.
+     * @note Assumes that 
+     */
+    class IAstNode {
+    public:
+        virtual ~IAstNode() = default;
+
+        virtual AstNodeType getType() const = 0;
+        virtual AstOpType getOp() const = 0;
+        virtual std::any acceptVisitor(IAstVisitor<std::any>& visitor) const = 0;
+    };
+}
+
+#endif
